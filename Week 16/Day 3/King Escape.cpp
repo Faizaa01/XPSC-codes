dbe96517153dd04c37e288089c22d9c38ad90d06
Faizaa01 <166419561+Faizaa01@users.noreply.github.com>
2025-02-20 00:03:43 +0600

    Update King Escape.cpp

--- Week 16/Day 3/King Escape.cpp
+++ Week 16/Day 3/King Escape.cpp
@@ -12,22 +12,22 @@ int main()
     int n;
     cin>>n;
     int a[2],b[2],c[2];
-    char x[2],y[2];
+    int x[2],y[2];
     cin>>a[0]>>a[1];
     cin>>b[0]>>b[1];
     cin>>c[0]>>c[1];
     
-    if(b[0]>a[0]) x[0]='r';
-    else x[0]='l';
+    if(b[0]>a[0]) x[0]=0;
+    else x[0]=1;
     
-    if(b[1]>a[1]) x[1]='u';
-    else x[1]='d';
+    if(b[1]>a[1]) x[1]=0;
+    else x[1]=1;
     
-    if(c[0]>a[0]) y[0]='r';
-    else y[0]='l';
+    if(c[0]>a[0]) y[0]=0;
+    else y[0]=1;
     
-    if(c[1]>a[1]) y[1]='u';
-    else y[1]='d';
+    if(c[1]>a[1]) y[1]=0;
+    else y[1]=1;
     
     if(x[0]==y[0] && x[1]==y[1]) yes;
     else no;
